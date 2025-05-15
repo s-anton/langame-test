78435629d6b5f7ea4a8f977bc9f9b46a4cf5d026
Syuskov Anton <anton.syuskov@gmail.com>
2025-05-16 01:09:48 +0300

    show alert block onMessage

--- assets/controllers/app_controller.js
+++ assets/controllers/app_controller.js
@@ -1,17 +1,39 @@
 import { Controller } from '@hotwired/stimulus';
-import { connectStreamSource, disconnectStreamSource } from "@hotwired/turbo";
 
 export default class extends Controller {
     static values = { mercureUrl: String }
     connect() {
         this.es = new EventSource(this.mercureUrlValue);
-        connectStreamSource(this.es);
+        this.es.addEventListener('message', this.onMessage.bind(this));
     }
 
     disconnect() {
         if (this.es) {
+            this.es.removeEventListener('message', this.onMessage);
             this.es.close();
-            disconnectStreamSource(this.es);
         }
+    }
+
+    onMessage(ev) {
+        const data = JSON.parse(ev.data);
+        let content = '';
+        switch (data.type) {
+            case 'user_created':
+                content = 'User was created: ' + data.data.username;
+                break;
+            case 'user_verified':
+                content = 'User was verified: ' + data.data.username;
+                break;
+            case 'news-entry_created':
+                content = 'News entry was added: ' + data.data.content;
+                break;
+            default:
+                content = 'Unknown type: ' + JSON.stringify(data.data);
+        }
+        this.element.innerHTML = `<div class="alert">${content}</div>`;
+
+        setTimeout(() => {
+            this.element.innerHTML = '';
+        }, 10000)
     }
 }
