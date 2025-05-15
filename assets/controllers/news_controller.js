6c4ae82b1044db8bec8e4cbda09bbdc328bc42cd
Syuskov Anton <anton.syuskov@gmail.com>
2025-05-15 23:59:28 +0300

    send search query

--- assets/controllers/news_controller.js
+++ assets/controllers/news_controller.js
@@ -2,21 +2,25 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
     static values = { newsListUrl: String };
+    static targets = [ "search" ]
 
     connect() {
-        if (document.documentElement.hasAttribute('data-turbo-preview')) {
-            return;
-        }
-        this.load('forward', '2030-01-01');
+        this.initList();
     }
 
     load(direction, lastPublishedAt) {
-        fetch(this.newsListUrlValue + '?' + (new URLSearchParams({direction, lastPublishedAt})))
+        const query = this.hasSearchTarget ? this.searchTarget.value : '';
+        const params = (new URLSearchParams({direction, lastPublishedAt, query}));
+        fetch(this.newsListUrlValue + '?' + params)
             .then(response => response.text())
             .then(html => this.element.innerHTML = html)
     }
 
     loadNews({params: {direction, lastPublishedAt}}) {
         this.load(direction, lastPublishedAt);
+    }
+
+    initList() {
+        this.load('forward', '2030-01-01');
     }
 }
